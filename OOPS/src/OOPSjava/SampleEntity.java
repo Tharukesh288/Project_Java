c111a4ca386f88748c8d56e62ecc5d5ce186c981
Tharukesh <tharukesh321@gmail.com>
2025-07-30 11:20:59 +0530

    To upload my project

---
+++ OOPS/src/OOPSjava/SampleEntity.java
@@ -0,0 +1,16 @@
+package OOPSjava;
+
+public class SampleEntity {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD!");
+		method();
+		
+	}
+	public static void method() {
+		method();
+	}
+	
+	
+
+}
